c263d7503842d0059dc92e02d893b11edfc5263e
Justine Casiano <justinecasiano30@gmail.com>
2023-08-05 22:58:58 +0800

    add initial html and css files and setup directory

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+// Function Calls
+isOnDesktop();
+changeAttraction(0);
+
+// Functions
+async function changeAttraction(index) {
+	const attractions = ['El Nido', 'Puerto Princesa', 'Coron', 'Port Barton', 'Balabac', 'Taytay'];
+	const selectors = ['.background', '.text-region', '.text-attraction', '.text-description']
+	const animations = ['fadeIn', 'fadeOut'];
+
+	if (index == attractions.length) index = 0;
+	const attraction = attractions[index];
+	const elements = getElements([], selectors);
+	setAttraction(attraction, elements[0]);
+
+	await toggleAnimation(elements, animations[0], 5000);
+	await toggleAnimation(elements, animations[1], 1900);
+	removeAnimation(elements, animations);
+	changeAttraction(++index);
+}
+
+function setAttraction(name, background) {
+	background.src = `images/${name}.jpg`;
+	name = name.split(' ');
+
+	const attractionName = document.querySelector('.text-attraction');
+	if (name.length > 1) name = [name.join(' ')];
+	attractionName.innerHTML = name;
+}
+
+function getElements(elements, selectors) {
+	for (const selector of selectors) {
+		elements.push(document.querySelector(selector));
+	}
+	return elements;
+}
+
+function toggleAnimation(elements, animation, time) {
+	return new Promise((resolve, reject) => {
+		elements.forEach(element => {
+			element.classList.toggle(animation);
+		});
+		setTimeout(() => resolve(), time);
+	});
+}
+
+function removeAnimation(elements, animations) {
+	elements.forEach(element => {
+		animations.forEach(animation => {
+			element.classList.remove(animation);
+		})
+	});
+}
+
+function isOnDesktop() {
+	const mediaQuery = window.matchMedia('(min-width: 768px)')
+	if (mediaQuery.matches) {
+		changeHeaderColorOnSroll();
+	}
+}
+
+function changeHeaderColorOnSroll() {
+	window.addEventListener('scroll', () => {
+		const nav = document.querySelector('nav');
+		const header = document.querySelector('header');
+		const isScrolling = window.scrollY >= window.innerHeight - header.clientHeight / 2;
+		nav.classList.toggle('scrolled', isScrolling);
+	});
+}
